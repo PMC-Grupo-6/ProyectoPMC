be47f18e189f2c30ecc0393add5af7fdcb5b506a
nplata1812 <53950395+nplata1812@users.noreply.github.com>
2020-10-26 02:01:05 -0500

    Firebase crea usuario

--- src/components/login.js
+++ src/components/login.js
@@ -1,108 +1,38 @@
 import React,{useState} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import 'firebase/auth'
-import {useFirebaseapp} from 'reactfire';
+import {useFirebaseApp} from 'reactfire';
+import { Button } from '@material-ui/core';
 
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+
 
 export default function Login() {
-  const classes = useStyles();
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
-  
-  function onChange ()  {
-    setEmail(document.getElementById("demo").value)
-    console.log(email)
-}
- 
-    
- 
+  const [mail,setEmail]=useState('');
+  const [contra,setPassword]=useState('');
+  const firebase=useFirebaseApp();
 
- 
+  const submit = async()=>{
+    while(mail==null && contra==null){
+
+    } 
+    firebase.auth().createUserWithEmailAndPassword(mail.trim(),contra);
+  }
+  const logOut=async()=>{
+    await firebase.auth().signOut();
+  }
+
 
 
   return (
     
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Iniciar Sesión
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Correo Electronico"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              
-            </Grid>
-            <Grid item>
-
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+   <div>
+     <div>
+       <label htmlFor="email">Correo Electronico</label>
+       <input type="email" id="email" onChange={(ev)=>setEmail(ev.target.value)}/>
+       <label htmlFor="email">Contraseña</label>
+       <input type="password" id="password" onChange={(ev)=>setPassword(ev.target.value)}/>
+       <button onClick={submit}>Iniciar sesion</button>
+       </div>
+       </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+
+var firebaseConfig={
+    apiKey: "AIzaSyCjiSpaMdXlyPl4eNlMoKoXRfjkk8qi6Us",
+        authDomain: "proyectopmc-c6b20.firebaseapp.com",
+        databaseURL: "https://proyectopmc-c6b20.firebaseio.com",
+        projectId: "proyectopmc-c6b20",
+        storageBucket: "proyectopmc-c6b20.appspot.com",
+        messagingSenderId: "683390242781",
+        appId: "1:683390242781:web:401a0842fc8745fc30b034",
+        measurementId: "G-VJ4NJ4Q4V8"
+    
+     
+}
+const fire=firebase.initializeApp(firebaseConfig);
+export default fire;
