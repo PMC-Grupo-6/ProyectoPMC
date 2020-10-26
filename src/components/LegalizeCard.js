636386479e02b8d0a54f6db44cd7cdf9fbde0b28
nplata1812 <53950395+nplata1812@users.noreply.github.com>
2020-10-26 14:47:24 -0500

    Firebase

--- src/components/LegalizeCard.js
+++ src/components/LegalizeCard.js
@@ -3,9 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Stepper from "./Stepper"
-import { useFirebaseApp } from "reactfire";
 import { useHistory } from "react-router-dom";
-
+import {auth} from "../firebase-config" 
 const useStyles = makeStyles({
   root: {
     width: "100%",
@@ -18,9 +17,9 @@ const useStyles = makeStyles({
 export default function MediaCard() {
   const classes = useStyles();
   const history = useHistory()
-  const firebase = useFirebaseApp();
 
-  if(firebase.auth().currentUser === null){
+
+  if(auth.currentUser === null){
     history.push("/login")
   }
 
