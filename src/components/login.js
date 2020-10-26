be06b00cb8a6448b76f054c1bd0286e2ee0118da
Mario-Hurtado <ma.hurtados@uniandes.edu.co>
2020-10-26 03:12:03 -0500

    Login terminado

--- src/components/login.js
+++ src/components/login.js
@@ -1,38 +1,126 @@
-import React,{useState} from 'react';
-import 'firebase/auth'
-import {useFirebaseApp} from 'reactfire';
-import { Button } from '@material-ui/core';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useFirebaseApp } from "reactfire";
+import Footer from "./Footer";
 
-
-
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  enviar: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  footer: {
+    display: "flex",
+    minHeight: "100vh",
+    flexDirection: "column",
+  },
+}));
 
 export default function Login() {
-  const [mail,setEmail]=useState('');
-  const [contra,setPassword]=useState('');
-  const firebase=useFirebaseApp();
-
-  const submit = async()=>{
-    while(mail==null && contra==null){
-
-    } 
-    firebase.auth().createUserWithEmailAndPassword(mail.trim(),contra);
-  }
-  const logOut=async()=>{
+  const classes = useStyles();
+  const [mail, setEmail] = useState("");
+  const [contra, setPassword] = useState("");
+  const firebase = useFirebaseApp();
+  const submit = async () => {
+    while (mail == null && contra == null) {}
+    firebase.auth().createUserWithEmailAndPassword(mail.trim(), contra);
+  };
+  const logOut = async () => {
     await firebase.auth().signOut();
-  }
-
-
+  };
 
   return (
-    
-   <div>
-     <div>
-       <label htmlFor="email">Correo Electronico</label>
-       <input type="email" id="email" onChange={(ev)=>setEmail(ev.target.value)}/>
-       <label htmlFor="email">Contraseña</label>
-       <input type="password" id="password" onChange={(ev)=>setPassword(ev.target.value)}/>
-       <button onClick={submit}>Iniciar sesion</button>
-       </div>
-       </div>
+    <div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Iniciar Sesión
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo Electrónico"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Recordarme"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.enviar}
+              onClick={submit}
+            >
+              Iniciar Sesión
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Olvidaste tu constraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"No tienes una cuenta? Regístrate"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+      <div className={classes.footer}>
+        <Footer />
+      </div>
+    </div>
   );
 }
