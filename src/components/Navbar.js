71a21d5d5cfedb861475f43288f8fa9a2a98e1cc
Antonio Aspite <antonioaspite@gmail.com>
2020-11-11 14:17:54 -0500

    cambiar boton de iniciar sesion cuando está logueado el usuario

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import DescriptionIcon from '@material-ui/icons/Description';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import DescriptionIcon from "@material-ui/icons/Description";
+import { auth } from "../firebase-config";
 
-import {
-  Link,
-} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,43 +17,56 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  
+
   title: {
     flexGrow: 1,
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
   },
   inicio: {
     marginLeft: theme.spacing(2),
-    color:"black"
+    color: "black",
   },
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  var user = auth.currentUser;
 
   return (
- 
     <div className={classes.root}>
-      <AppBar color="white"position="static">
-        <Toolbar> 
-          <IconButton edge="start" className={classes.menuButton} color="primary" aria-label="menu">
-            <DescriptionIcon  />
+      <AppBar color="white" position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="primary"
+            aria-label="menu"
+          >
+            <DescriptionIcon />
           </IconButton>
           <div className={classes.title}>
-          <Typography variant="h6" >
-            Clack
-          </Typography>
-          <Button style={{marginLeft: "16px"}} component={Link}  variant="outlined" to="/">
-            Legalizar
-          </Button> 
+            <Typography variant="h6">Clack</Typography>
+            <Button
+              style={{ marginLeft: "16px" }}
+              component={Link}
+              variant="outlined"
+              to="/"
+            >
+              Legalizar
+            </Button>
           </div>
-          <Button component={Link} variant="contained" color="primary" className={classes.inicio} to="/login">
-            Iniciar Sesión
-          </Button>    
+          <Button
+            component={Link}
+            variant="contained"
+            color="primary"
+            className={classes.inicio}
+            to="/login"
+          >
+            {user ? "Mi cuenta" : "Iniciar Sesión"}
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
-    
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -12,10 +12,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Footer from "./Footer";
 import { useHistory } from "react-router-dom";
-import {auth} from "../firebase-config" 
-import { useAlert } from 'react-alert'
-
-
+import { auth } from "../firebase-config";
+import { useAlert } from "react-alert";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,56 +44,53 @@ export default function Login() {
   const classes = useStyles();
   const [mail, setEmail] = useState("");
   const [contra, setPassword] = useState("");
-  const history = useHistory()
-  const alert = useAlert()
+  const history = useHistory();
+  const alert = useAlert();
 
   const submit = async () => {
-    try{
-      auth.createUserWithEmailAndPassword(mail.trim(), contra)
-      alert.show("Registro exitoso")
+    try {
+      auth.createUserWithEmailAndPassword(mail.trim(), contra);
+      alert.show("Registro exitoso");
+    } catch (error) {
+      switch (error.code) {
+        case "auth/email-already-in-use":
+          alert.show("El correo electronico ya está en uso");
+          break;
+        case "auth/invalid-email":
+          alert.show("Debe ingresar un email valido");
+          break;
+        case "auth/invalid-password":
+          alert.show("La contraseña es invalida");
+          break;
+        case "auth/weak-password":
+          alert.show("La contraseña es debil");
+          break;
+        default:
+          alert.show(error.message);
+          break;
+      }
     }
-    catch(error){
-     switch(error.code) {
-          case 'auth/email-already-in-use':
-                alert.show("El correo electronico ya está en uso")
-                break;
-          case 'auth/invalid-email':
-                alert.show("Debe ingresar un email valido")
-                break;
-          case 'auth/invalid-password':
-                alert.show("La contraseña es invalida")
-                break;
-          case 'auth/weak-password':
-                alert.show("La contraseña es debil")
-                break;
-          default:
-                 alert.show(error.message)
-                  break;
-                  
-       }
-       
-      
-    }
-   
+    history.push("/");
   };
 
   const signIn = async () => {
-    auth.signInWithEmailAndPassword(mail.trim(), contra).catch(error=>{
-      console.log(error.code)
-      switch(error.code) {
-        case 'auth/user-not-found':
-              alert.show("Debe registrarse para poder iniciar sesion")
-              break;
-        case 'auth/wrong-password':
-              alert.show("La contraseña no es correcta")
-              break;
-        default:
-          alert.show(error.message)
-          break;
-     }
-
-    });
-    history.push("/")
+    await auth
+      .signInWithEmailAndPassword(mail.trim(), contra)
+      .catch((error) => {
+        console.log(error.code);
+        switch (error.code) {
+          case "auth/user-not-found":
+            alert.show("Debe registrarse para poder iniciar sesion");
+            break;
+          case "auth/wrong-password":
+            alert.show("La contraseña no es correcta");
+            break;
+          default:
+            alert.show(error.message);
+            break;
+        }
+      });
+    history.push("/");
   };
 
   const logOut = async () => {
@@ -103,8 +98,6 @@ export default function Login() {
   };
 
   var user = auth.currentUser;
-
- 
 
   return (
     <div>
@@ -114,94 +107,83 @@ export default function Login() {
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          {
-            user  &&
+          {user && (
             <>
-             <Typography component="h3" >
-          Bienvenido a Clack 
-          </Typography>
-          <Typography component="h3" >
-          {user.email}
-        </Typography>
-        <Link to="/">
-            <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.enviar}
-            onClick={logOut}
-          >
-            <a  href="/" class="btn btn-success">Cerrar Sesion</a>
-          </Button>
-          </Link>
-          
-         
-        </>
-          }
-          {!user &&
-            <>
-          <Typography component="h1" variant="h5">
-            Iniciar Sesión
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Correo Electrónico"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(ev) => setEmail(ev.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-            <Link to="/">
+              <Typography component="h3">Bienvenido a Clack</Typography>
+              <Typography component="h3">{user.email}</Typography>
+              <Link to="/">
                 <Button
+                  type="submit"
                   fullWidth
                   variant="contained"
                   color="primary"
-                  onClick={signIn}
+                  className={classes.enviar}
+                  onClick={logOut}
                 >
-                  Iniciar Sesion
-                  
+                  <a href="/" class="btn btn-success">
+                    Cerrar Sesion
+                  </a>
                 </Button>
-                <br/>
-                <br/>
-                <Button
-                  fullWidth
+              </Link>
+            </>
+          )}
+          {!user && (
+            <>
+              <Typography component="h1" variant="h5">
+                Iniciar Sesión
+              </Typography>
+              <form className={classes.form} noValidate>
+                <TextField
                   variant="outlined"
-                  color="primary"
-                  onClick={submit}
-                >
-                  Registrarse
-                  
-                </Button>
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Correo Electrónico"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  onChange={(ev) => setEmail(ev.target.value)}
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Contraseña"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(ev) => setPassword(ev.target.value)}
+                />
+                <Link to="/">
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={signIn}
+                  >
+                    Iniciar Sesion
+                  </Button>
+                  <br />
+                  <br />
+                  <Button
+                    fullWidth
+                    variant="outlined"
+                    color="primary"
+                    onClick={submit}
+                  >
+                    Registrarse
+                  </Button>
                 </Link>
-            <Grid container>
-              <Grid item xs>
-                
-              </Grid>
-              <Grid item>
-               
-              </Grid>
-            </Grid>
-          </form>
-          </>
-          }
+                <Grid container>
+                  <Grid item xs></Grid>
+                  <Grid item></Grid>
+                </Grid>
+              </form>
+            </>
+          )}
         </div>
       </Container>
       <div className={classes.footer}>
